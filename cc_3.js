b539c25be8157dc6659cf790233683f3951086ad
Alex Cleveland <acleveland2@usf.edu>
2024-09-07 20:03:01 -0400

    Create determinePerformanceRating function.

--- cc_3.js
+++ cc_3.js
@@ -5,3 +5,16 @@ function calculateAverageSales(sales){
     const totalSales = sales.reduce((sum, sale)=> sum +sale, 0);
     return totalSales / sales.length;
 }
+// Task 2 Create a function to determine performance rating 
+
+function determinePerformanceRating(averageSales){
+    if (averageSales > 10000)
+     return "Excellent";
+ if (averageSales >= 7000)
+     return "Good";
+ if (averageSales >= 4000)
+     return "Satisfactory";
+ if (averageSales > 4000)
+ return"Needs Improvement";
+ }
+ 
